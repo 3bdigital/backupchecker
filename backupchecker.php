a089185761436060b96fcad9c2644f95e5ab5bbb
Jordan Worner <jordan.worner@gmail.com>
2013-08-16 15:48:09 +0100

    saves and loads settings

--- backupchecker.php
+++ backupchecker.php
@@ -2,9 +2,15 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-function storeSettings($settings) {
-	$settingsFile = 'backupcheckersettings.json';
-	file_put_contents($settingsFile, $settings);
+function loadSettings($file) {
+	$data = file_get_contents($file);
+	if(!$data) return array();
+	$json = json_decode($data);
+	return $json;
+}
+
+function storeSettings($settings, $file) {
+	file_put_contents($file, $settings);
 }
 
 function checkGmail($username, $password, $label = '') { 
@@ -26,6 +32,8 @@ function checkGmail($username, $password, $label = '') {
 	return $mailData;
 }
 
+$settingsFile = 'backupcheckersettings.json';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// store settings
 	$settings = array();
@@ -39,63 +47,67 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$titles = $_POST['titles'];
 
 	foreach($labels as $key => $value) {
-		$settings['labels'][] = array('label' => $value, 'title' => $titles[$key]);
+		$settings['labels'][] = array('label' => $value, 'title' => $titles[$key], 'success' => array());
 	}
 
 	$json = json_encode($settings);
 
-	storeSettings($json);
+	storeSettings($json, $settingsFile);
 }
 
-$username = ''; // google username
-$password = ''; // google password
-$days = 7; // the amount of days to check
-$nestedLabel = ''; // if the label is nested add the parent labels ending with "-" e.g. the nested label backup/example would be "backup-"
+$settings = loadSettings($settingsFile);
 
-$clients = array(
-	'bnms' 				=> array('name' => 'BNMS', 'success' => array()),
-	'3b' 				=> array('name' => '3B', 'success' => array()),
-	'siliconjunction' 	=> array('name' => 'Silicon Junction', 'success' => array()),
-	'oldbrightonians' 	=> array('name' => 'Old Brightonians', 'success' => array()),
-	'rapidaddition' 	=> array('name' => 'Rapid Addition', 'success' => array()),
-	'crosfields' 		=> array('name' => 'Crosfields', 'success' => array()),
-	'fepsa' 			=> array('name' => 'FESPA', 'success' => array()),
-	'marshallarts' 		=> array('name' => 'Marshall Arts', 'success' => array()),
-);
+$username = (isset($settings->username)) ? $settings->username : '';
+$password = (isset($settings->password)) ? $settings->password : '';
+$days = (isset($settings->days)) ? $settings->days : '';
+$nestedLabel = (isset($settings->nested)) ? $settings->nested : '';
+$labels = (isset($settings->labels)) ? $settings->labels : array();
+
+$check = true;
+$msg = '';
+
+if(empty($username) || empty($password) || empty($days) || empty($nestedLabel)) {
+	$check = false;
+	$msg = 'Please check your settings';
+}
 
 // process the clients rss
-// foreach($clients as $key => $value) {
-// 	$label = $key;
-// 	if($nestedLabel) $label = $nestedLabel . $label;
-// 	$feed = checkGmail($username, $password, $label);
-// 	$xml = new SimpleXMLElement($feed);
+foreach($labels as $key => $value) {
+	$label = $key;
+	if($nestedLabel) $label = $nestedLabel . $label;
+	$feed = checkGmail($username, $password, $label);
+	$xml = new SimpleXMLElement($feed);
 
-// 	// check for valid XML
-// 	if($xml) {
-// 		// make sure there is some emails
-// 		if($xml->fullcount > 0) {
-// 			// grab all the email entries
-// 			$entries = $xml->entry;
-// 			// today at midnight
-// 			$today = new DateTime(date('Y-m-d') . ' 23:59:59');
-// 			$maxTime = 86400; // 24 hours in seconds
-// 			for($i=0; $i<$days; $i++) {
-// 				if(isset($entries[$i])) {
-// 					$entry = $entries[$i];
-// 					// move the base date
-// 					$today->modify('-' . $i . ' days');
-// 					// get the email date
-// 					$emailDate = new DateTime($entry->issued);
-// 					// compare the dates
-// 					$diff = $today->getTimestamp() - $emailDate->getTimestamp();
-// 					// if the difference is less than 24 hours then a backup has been run for that day
-// 					if($diff < $maxTime) $clients[$key]['success'][$i] = true;
-// 				}
-// 			}
+	// check for valid XML
+	if($xml) {
+		// make sure there is some emails
+		if($xml->fullcount > 0) {
+			// grab all the email entries
+			$entries = $xml->entry;
+			// today at midnight
+			$today = new DateTime(date('Y-m-d') . ' 23:59:59');
+			$maxTime = 86400; // 24 hours in seconds
+			for($i=0; $i<$days; $i++) {
+				if(isset($entries[$i])) {
+					$entry = $entries[$i];
+					// move the base date
+					$today->modify('-' . $i . ' days');
+					// get the email date
+					$emailDate = new DateTime($entry->issued);
+					// compare the dates
+					$diff = $today->getTimestamp() - $emailDate->getTimestamp();
+					// if the difference is less than 24 hours then a backup has been run for that day
+					if($diff < $maxTime) $clients[$key]['success'][$i] = true;
+				}
+			}
 
-// 		}
-// 	}
-// }
+		}
+	} else {
+		$check = false;
+		$msg = 'Username or password is incorrect';
+		break;
+	}
+}
 
 ?>
 <!DOCTYPE html>
@@ -144,6 +156,12 @@ $clients = array(
 
     <div class="container">
 
+    	<?php if(!$check): ?>
+		<div class="alert alert-danger">
+			<?php echo $msg; ?>
+		</div>
+    	<?php endif; ?>
+
 		<h1>Backup Checker</h1>
 
 		<p><button type="button" id="toggle-settings" class="btn btn-primary btn-xs">Toggle Settings</button></p>
@@ -154,13 +172,13 @@ $clients = array(
 					<div class="col-lg-3">
 						<div class="form-group">
 							<label for="username">Username</label>
-							<input type="text" class="form-control" name="username" id="username" placeholder="Enter username">
+							<input type="text" class="form-control" name="username" id="username" placeholder="Enter username" value="<?php echo $settings->username; ?>">
 						</div>
 					</div>
 					<div class="col-lg-3">
 						<div class="form-group">
 							<label for="password">Password</label>
-							<input type="text" class="form-control" name="password" id="password" placeholder="Enter password">
+							<input type="text" class="form-control" name="password" id="password" placeholder="Enter password"  value="<?php echo $settings->password; ?>">
 						</div>
 					</div>
 					<div class="col-lg-3">
@@ -168,7 +186,7 @@ $clients = array(
 							<label for="password">Days</label>
 							<select class="form-control" name="days" id="days">
 								<?php for($i=1; $i<15; $i++): ?>
-								<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+								<option value="<?php echo $i; ?>"<?php if($i == $settings->days) echo ' selected="selected"'; ?>><?php echo $i; ?></option>
 								<?php endfor; ?>
 							</select>
 						</div>
@@ -176,24 +194,26 @@ $clients = array(
 					<div class="col-lg-3">
 						<div class="form-group">
 							<label for="nested">Nested Labels</label>
-							<input type="text" class="form-control" name="nested" id="nested" placeholder="Enter nested label">
+							<input type="text" class="form-control" name="nested" id="nested" placeholder="Enter nested label"  value="<?php echo $settings->nested; ?>">
 						</div>
 					</div>
 				</div>
 				<label>Labels</label>
 				<div id="labels">
+					<?php foreach($settings->labels as $label): ?>
 					<div class="row">
 						<div class="col-lg-3">
 							<div class="form-group">
-								<input type="text" class="form-control" name="labels[]">
+								<input type="text" class="form-control" name="labels[]" value="<?php echo $label->label; ?>">
 							</div>
 						</div>
 						<div class="col-lg-3">
 							<div class="form-group">
-								<input type="text" class="form-control" name="titles[]">
+								<input type="text" class="form-control" name="titles[]" value="<?php echo $label->title; ?>">
 							</div>
 						</div>
 					</div>
+					<?php endforeach; ?>
 				</div>
 				<p><button type="submit" id="add-label" class="btn btn-xs btn-default">Add Label</button></p>
 				<button type="submit" class="btn btn-default btn-success">Submit</button>
@@ -211,10 +231,10 @@ $clients = array(
 	          </tr>
 	        </thead>
 	        <tbody>
-	          <?php foreach($clients as $client): ?>
+	          <?php foreach($labels as $label): ?>
 	          <tr>
 	            <td width="5%">1</td>
-	            <td width="25%"><?php echo $client['name']; ?></td>
+	            <td width="25%"><?php echo $label->title; ?></td>
 	            <? for ($i=0; $i<$days; $i++): ?>
 				<td width="10%"><span class="glyphicon glyphicon-<?php echo (isset($client['success'][$i])) ? 'ok' : 'remove' ?>"></span></td>
 				<?php endfor; ?>
